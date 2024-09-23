256ffa9441433f562f4a6b820eed5b65dd2766f4
martinjs-dev <martindohou.pro@gmail.com>
2024-09-23 14:32:18 +0100

    [UPDATE] Linting

--- next-movie/next.config.js
+++ next-movie/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/",
-                destination: "/discover/now_playing",
+                destination: "/movies",
                 permanent: true
             },
         ];
